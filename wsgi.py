b346c4ebb1f39d5575bc6a127047e16948958951
pmflypallavi <58667171+pmflypallavi@users.noreply.github.com>
2019-12-31 00:06:02 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 application = Flask(__name__)
+application.config['MYSQL_HOST'] = 'custom-mysql.gamification.svc.cluster.local'
+application.config['MYSQL_USER'] = 'xxuser'
+application.config['MYSQL_PASSWORD'] = 'welcome1'
+application.config['MYSQL_DB']= 'sampledb'
+mysql = MySQL(application)
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def employees():
+    try:
+        cur = mysql.connection.cursor()
+        print(cur)
+        res = cur.execute("SELECT ITEM_NUMBER, DESCRIPTION, LONG_DESCRIPTION FROM sampledb.XXIBM_PRODUCT_STYLE LIMIT 10")
+        print(res)
+        if res > 0:
+            userDetails = cur.fetchall()
+            return render_template('employee.html',userDetails=userDetails)
+        cur.close()
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
     application.run()
